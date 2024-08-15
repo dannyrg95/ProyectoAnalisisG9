30be0ea6d58f54788cf9c121f3bd4852ab9af10c
dannyrg95 <dannyrg-@hotmail.com>
2024-08-15 16:29:18 -0600

    Version Inicial

---
+++ ChemasfutCR_API/ChemasfutCR_API/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChemasfutCR_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        [Route("ConsultarUsuarios")]
+        public IActionResult ConsultarUsuarios()
+        {
+            var resultado = 750000;
+
+            return Ok(resultado);
+        }
+
+    }
+}
