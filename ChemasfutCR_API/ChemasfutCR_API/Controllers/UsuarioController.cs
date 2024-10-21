bbc465641315f05d3364460eac5dee689e02d9c5
dannyrg95 <dannyrg-@hotmail.com>
2024-10-20 21:13:18 -0600

    Creacion Usuario Entidad
    Para UsuarioController Registro Usuario

--- ChemasfutCR_API/ChemasfutCR_API/Controllers/UsuarioController.cs
+++ ChemasfutCR_API/ChemasfutCR_API/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using ChemasfutCR_API.Entities;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
@@ -11,11 +12,13 @@ namespace ChemasfutCR_API.Controllers
     {
         [HttpPost]
         [Route("RegistrarUsuario")]
-        public IActionResult RegistrarUsuario()
+        public IActionResult RegistrarUsuario(Usuario entidad)
         {
             using(var context = new SqlConnection("Server=LIED95\\SQLEXPRESS;Database=ChemasfutCR;Trusted_Connection=True;TrustServerCertificate=True;"))
             {
-                var result = context.Execute("RegistrarUsuario", new { }, commandType: System.Data.CommandType.StoredProcedure);
+                var result = context.Execute("RegistrarUsuario", 
+                    new { entidad.Nombre, entidad.Apellido, entidad.Fecha_Nacimiento, entidad.Teléfono, entidad.Email, entidad.Contraseña, entidad.Identificacion}, 
+                    commandType: System.Data.CommandType.StoredProcedure);
             }
 
             return Ok("Todo bien");
---
+++ ChemasfutCR_API/ChemasfutCR_API/Entities/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace ChemasfutCR_API.Entities
+{
+    public class Usuario
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Fecha_Nacimiento { get; set; }
+        public string? Teléfono { get; set; }
+        public string? Email { get; set; }
+        public string? Contraseña { get; set; }
+        public string? Identificacion { get; set; }
+    }
+}
