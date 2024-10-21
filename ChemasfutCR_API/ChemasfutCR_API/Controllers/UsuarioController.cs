59c7702661985bea2adf5044ac8719be835154ae
dannyrg95 <dannyrg-@hotmail.com>
2024-10-20 22:21:08 -0600

    IniciarSesion API
    Metodo iniciar sesion en UsuarioController dentro del API

--- ChemasfutCR_API/ChemasfutCR_API/Controllers/UsuarioController.cs
+++ ChemasfutCR_API/ChemasfutCR_API/Controllers/UsuarioController.cs
@@ -12,17 +12,61 @@ namespace ChemasfutCR_API.Controllers
     {
         [HttpPost]
         [Route("RegistrarUsuario")]
-        public IActionResult RegistrarUsuario(Usuario entidad)
+        public async Task<IActionResult> RegistrarUsuario(Usuario entidad)
         {
-            using(var context = new SqlConnection("Server=LIED95\\SQLEXPRESS;Database=ChemasfutCR;Trusted_Connection=True;TrustServerCertificate=True;"))
+            Respuesta resp = new Respuesta();
+
+            using (var context = new SqlConnection("Server=LIED95\\SQLEXPRESS;Database=ChemasfutCR;Trusted_Connection=True;TrustServerCertificate=True;"))
             {
-                var result = context.Execute("RegistrarUsuario", 
+                var result = await context.ExecuteAsync("RegistrarUsuario", 
                     new { entidad.Nombre, entidad.Apellido, entidad.Fecha_Nacimiento, entidad.Teléfono, entidad.Email, entidad.Contraseña, entidad.Identificacion}, 
                     commandType: System.Data.CommandType.StoredProcedure);
-            }
 
-            return Ok("Todo bien");
-        } 
+                if (result > 0)
+                {
+                    resp.Codigo = 1;
+                    resp.Mensaje = "OK";
+                    resp.Contenido = true;
+                    return Ok(resp);
+                }                    
+                else
+                {
+                    resp.Codigo = 0;
+                    resp.Mensaje = "La informacion del usuario ya se encuentra registrada";
+                    resp.Contenido = false;
+                    return Ok("Error");
+                }
+            }
+        }
+
+        [HttpPost]
+        [Route("IniciarSesion")]
+        public async Task<IActionResult> IniciarSesion(Usuario entidad)
+        {
+            Respuesta resp = new Respuesta();
+
+            using (var context = new SqlConnection("Server=LIED95\\SQLEXPRESS;Database=ChemasfutCR;Trusted_Connection=True;TrustServerCertificate=True;"))
+            {
+                var result = await context.QueryAsync<Usuario>("IniciarSesion",
+                    new { entidad.Email, entidad.Contraseña},
+                    commandType: System.Data.CommandType.StoredProcedure);
+
+                if (result.Count() > 0)
+                {
+                    resp.Codigo = 1;
+                    resp.Mensaje = "OK";
+                    resp.Contenido = result;
+                    return Ok(resp);
+                }
+                else
+                {
+                    resp.Codigo = 0;
+                    resp.Mensaje = "La informacion del usuario ya se encuentra registrada";
+                    resp.Contenido = false;
+                    return Ok("Error");
+                }
+            }
+        }
 
     }
 }
---
+++ ChemasfutCR_API/ChemasfutCR_API/Entities/Respuesta.cs
@@ -0,0 +1,9 @@
+﻿namespace ChemasfutCR_API.Entities
+{
+    public class Respuesta
+    {
+        public int Codigo { get; set; } = 0;
+        public string? Mensaje { get; set; } = "Ok";
+        public object? Contenido { get; set; }
+    }
+}
