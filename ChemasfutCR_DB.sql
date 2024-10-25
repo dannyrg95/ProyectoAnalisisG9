73d2c766f7f8b88abd8091e0bba34e83772d6c0e
Jesus <jesusjm12@outlook.es>
2024-10-24 23:47:55 -0600

    Improving DB script (focused on Usuario)

--- ChemasfutCR_DB.sql
+++ ChemasfutCR_DB.sql
@@ -1,28 +1,35 @@
 /****** TABLAS ******/
 
 /** USUARIO **/
-CREATE TABLE Usuario(
-	ID_Usuario INT PRIMARY KEY NOT NULL,
+CREATE TABLE Usuario (
+	ID_Usuario INT IDENTITY(1,1) PRIMARY KEY,
 	Nombre VARCHAR(100) NOT NULL,
-	Apellido VARCHAR(100) NOT NULL,
+	Apellido VARCHAR(100) NULL,
+    Identificacion INT NULL,
 	Fecha_Nacimiento DATE NOT NULL,
-	Teléfono VARCHAR(20) NOT NULL,
-	Email VARCHAR(100) NOT NULL,
-	Contraseña VARCHAR(100) NOT NULL,
-	ID_Rol INT NULL,
-	Estado BIT NOT NULL
-	);
+	Telefono VARCHAR(20) NOT NULL,
+	Email VARCHAR(255) NOT NULL,
+	Password VARCHAR(100) NOT NULL,
+	ID_Rol INT,
+	Estado BIT NOT NULL,
+    FOREIGN KEY (ID_Rol) REFERENCES Rol(ID_Rol)
+);
+
+CREATE TABLE Rol (
+    ID_Rol INT IDENTITY(1,1) PRIMARY KEY,
+    Descripcion VARCHAR(10)
+);
 
 /** CATEGORIA **/
 CREATE TABLE Categoria (
-    ID_Categoria INT PRIMARY KEY,
+    ID_Categoria INT IDENTITY(1,1) PRIMARY KEY,
     Nombre VARCHAR(50) NOT NULL,
-    Descripcion VARCHAR(100) NOT NULL,
+    Descripcion VARCHAR(100) NOT NULL
 );
 
 /** PRODUCTOS **/
 CREATE TABLE Productos(
-	ID_Producto INT PRIMARY KEY,
+	ID_Producto INT IDENTITY(1,1) PRIMARY KEY,
 	Nombre_Producto VARCHAR(100) NOT NULL,
 	Descripcion VARCHAR(100) NOT NULL,
 	Precio DECIMAL(10,2) NOT NULL,
@@ -31,53 +38,45 @@ CREATE TABLE Productos(
 	Talla VARCHAR(20),
 	Color VARCHAR(20),
 	FOREIGN KEY (ID_Categoria) REFERENCES Categoria(ID_Categoria)
-	);
+);
 
 /** PEDIDOS **/
-CREATE TABLE Pedidos(
-	ID_Pedido INT PRIMARY KEY,
+CREATE TABLE Pedidos (
+	ID_Pedido INT IDENTITY(1,1) PRIMARY KEY,
 	ID_Usuario INT,
+    ID_Producto INT,
 	Fecha_Pedido DATE NOT NULL,
-	Monto_Total DECIMAL(10,2) NOT NULL,
+    Cantidad INT NOT NULL,
+    Monto_Total DECIMAL(10,2) NOT NULL,
 	Metodo_Pedido VARCHAR(50),
 	Metodo_Pago VARCHAR(50),
-	FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
-	);
-
-/** DETALLES PEDIDO **/
-CREATE TABLE Detalles_Pedido(
-	ID_Detalle_Pedido INT PRIMARY KEY,
-    ID_Pedido INT,
-    ID_Producto INT,
-    Cantidad INT NOT NULL,
-    Precio DECIMAL(10,2) NOT NULL,
-    FOREIGN   
- KEY (ID_Pedido) REFERENCES Pedidos(ID_Pedido),
+	FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario),
     FOREIGN KEY (ID_Producto) REFERENCES Productos(ID_Producto)
-	);
+);
 
 /****** PROCEDIMIENTOS ALMACENADOS ******/
 
 /** REGISTRAR USUARIO **/
 CREATE PROCEDURE RegistrarUsuario
-	@Nombre				varchar(100),
-	@Apellido			varchar(100),
-	@Fecha_Nacimiento	date,
-	@Teléfono			varchar(20),
-	@Email				varchar(100),
-	@Contraseña			varchar(100),
-	@Identificacion		varchar(50)
+    @ID_Usuario         INT,
+	@Nombre				VARCHAR(100),
+	@Apellido			VARCHAR(100),
+	@Identificacion		VARCHAR(50),
+    @Fecha_Nacimiento	DATE,
+	@Telefono			VARCHAR(20),
+	@Email				VARCHAR(100),
+	@Password			VARCHAR(100),
+	@ID_Rol             INT
 AS
 BEGIN
 
-	DECLARE @Rol	TINYINT = 1,
-			@Estado	BIT		= 1
+	DECLARE @Estado	BIT	= 1
 
-	IF NOT EXISTS(SELECT 1 FROM dbo.Usuario WHERE Email = @Email OR Identificacion = @Identificacion)
+	IF NOT EXISTS(SELECT 1 FROM dbo.Usuario WHERE Email = @Email)
 	BEGIN
 
-		INSERT INTO dbo.Usuario(Nombre,Apellido,Fecha_Nacimiento,Teléfono,Email,Contraseña,Identificacion,ID_Rol,Estado)
-		VALUES (@Nombre,@Apellido,@Fecha_Nacimiento,@Teléfono,@Email,@Contraseña,@Identificacion,@Rol,@Estado)
+		INSERT INTO dbo.Usuario(ID_Usuario,Nombre,Apellido,Identificacion,Fecha_Nacimiento,Telefono,Email,Password,ID_Rol,Estado)
+		VALUES (@Nombre,@Apellido,@Identificacion,@Fecha_Nacimiento,@Telefono,@Email,@Password,@ID_Rol,@Estado)
 
 	END
 
@@ -92,19 +91,21 @@ CREATE PROCEDURE ActualizarUsuario
     @ID_Usuario INT,
     @Nombre VARCHAR(100),
     @Apellido VARCHAR(100),
+    @Identificacion INT,
     @Fecha_Nacimiento DATE,
-    @Teléfono VARCHAR(20),
-    @ID_Rol INT,
-    @Estado BIT
+    @Telefono VARCHAR(20),
+    @Email VARCHAR(255),
+    @Password VARCHAR(100)
 AS
 BEGIN
     UPDATE Usuario
     SET Nombre = @Nombre,
         Apellido = @Apellido,
+        Identificacion = @Identificacion,
         Fecha_Nacimiento = @Fecha_Nacimiento,
-        Teléfono = @Teléfono,
-        ID_Rol = @ID_Rol,
-        Estado = @Estado
+        Telefono = @Telefono,
+        Email = @Email,
+        Password = @Password,
     WHERE ID_Usuario = @ID_Usuario;
 END;
 
@@ -121,7 +122,7 @@ END;
 /** INICIAR SESION **/
 CREATE PROCEDURE IniciarSesion
     @Email       VARCHAR(100),
-    @Contraseña  VARCHAR(100)
+    @Contraseï¿½a  VARCHAR(100)
 AS
 BEGIN
     SET NOCOUNT ON;
@@ -139,37 +140,23 @@ BEGIN
         dbo.Rol R ON U.ID_Rol = R.ID_Rol
     WHERE 
         U.Email = @Email
-        AND U.Contraseña = @Contraseña
+        AND U.Contraseï¿½a = @Contraseï¿½a
         AND U.Estado = 1;
 END
-
-/** INSERTAR USUARIO **/
-CREATE PROCEDURE InsertarUsuario
-    @Nombre VARCHAR(100),
-    @Apellido VARCHAR(100),
-    @Fecha_Nacimiento DATE,
-    @Teléfono VARCHAR(20),
-    @Email VARCHAR(100),
-    @Contraseña VARCHAR(100),
-    @ID_Rol INT,
-    @Estado BIT
-AS
-BEGIN
-    INSERT INTO Usuario (Nombre, Apellido, Fecha_Nacimiento, Teléfono, Email, Contraseña, ID_Rol, Estado)
-    VALUES (@Nombre, @Apellido, @Fecha_Nacimiento, @Teléfono, @Email, @Contraseña, @ID_Rol, @Estado);
-END;
 
 /** OBTENER TODOS LOS USUARIOS **/
 CREATE PROCEDURE ObtenerTodosLosUsuarios
 AS
 BEGIN
-    SELECT ID_Usuario,
+    SELECT 
+        ID_Usuario,
         Nombre,
         Apellido,
+        Identificacion,
         Fecha_Nacimiento,
-        Teléfono,
+        Telefono,
         Email,
-        Contraseña,
+        Password,
         ID_Rol,
         Estado
 	FROM Usuario
@@ -186,10 +173,11 @@ BEGIN
         ID_Usuario,
         Nombre,
         Apellido,
+        Identificacion,
         Fecha_Nacimiento,
-        Teléfono,
+        Telefono,
         Email,
-        Contraseña,
+        Password,
         ID_Rol,
         Estado
     FROM 
